e1645c744d0b21ac92d6a5b839b975c7e8556955
Maykon Felix <equipefelixx@gmail.com>
2022-11-10 13:42:37 -0300

    Finalizado ( CRUD )
    Finalizado um Simples CRUD de cadastro de funcionario.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const btnSalvar = document.querySelector('#btnSalvar');
 let itens;
 let id;
 
-const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? [];
-const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens));
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 
 function loadItens() {
     itens = getItensBD()
@@ -27,14 +27,14 @@ function insertItem(item, index) {
     tr.innerHTML = `
     <td>${item.nome}</td>
     <td>${item.funcao}</td>
-    <td>${item.salario}</td>
+    <td>R$ ${item.salario}</td>
     <td class="acao">
-        <button onclik="editItem(${index})">i class='bx bx-edit'></i></button>
-        </td>
-        <td class="acao">
-        <button onclik="deleteItem(${index})">i class='bx bx-trash'></i></button>
-        </td>
-    `
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
     tbody.appendChild(tr)
 }
 
@@ -48,4 +48,45 @@ function deleteItem(index) {
     loadItens()
 }
 
-function openModal(edit = false, index = 0)
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active')
+
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        }
+    }
+
+    if (edit) {
+        sNome.value = itens[index].nome
+        sFuncao.value = itens[index].funcao
+        sSalario.value = itens[index].salario
+        id = index
+    } else {
+        sNome.value = ''
+        sFuncao.value = ''
+        sSalario.value = ''
+    }
+}
+
+btnSalvar.onclick = e => {
+    if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
+        return
+    }
+
+    e.preventDefault()
+
+    if (id !== undefined) {
+        itens[id].nome = sNome.value
+        itens[id].funcao = sFuncao.value
+        itens[id].salario = sSalario.value
+    } else {
+        itens.push({ 'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value })
+    }
+
+    setItensBD();
+
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+}
