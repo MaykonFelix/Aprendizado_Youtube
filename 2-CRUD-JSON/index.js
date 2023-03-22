084b4f6f050bf6f76430a681c5be93c333662948
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-21 23:24:35 -0300

    modified:   2-CRUD-JSON/index.js

--- 2-CRUD-JSON/index.js
+++ 2-CRUD-JSON/index.js
@@ -2,22 +2,27 @@ const express = require("express");
 const server = express();
 const router = express.Router();
 const fs = require("fs"); // File System ***Gerenciar Arquivo
-
 server.use(express.json({ extend: true }));
 
+// Função para converter o JSON para Object
 const readFile = () => {
-  const content = fs.readFileSync("./data/items.json", "utf-8");
+  const content = fs.readFileSync("./data/items.json", "utf-8"); //Função para pegar o JSON
   return JSON.parse(content);
 };
 
-/* Fazer a Leitura do  Arquivo JSON*/
+// Fazer a Leitura do  Arquivo JSON
 router.get("/", (req, res) => {
   const content = readFile();
   res.send(content);
 });
 
 router.post("/", (req, res) => {
-  res.send("Bem Vindo");
+  const { name, email, phone } = req.body;
+
+  const correntContent = readFile();
+  correntContent.push({ name, email, phone });
+  res.send(correntContent);
+  
 });
 
 router.put("/", (req, res) => {
