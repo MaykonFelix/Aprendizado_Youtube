19e7c6b3af8c632212ad10c0e0159a292d3bf5aa
Maykon Felix <equipefelixx@gmail.com>
2023-05-06 17:54:48 -0300

    Update

--- 7-MUI-Teste/src/App.tsx
+++ 7-MUI-Teste/src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <>
-      <p>Hello Word</p>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
