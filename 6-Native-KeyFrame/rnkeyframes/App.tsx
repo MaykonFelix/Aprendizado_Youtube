878491147ff720681b7177d7cf6f22e521a65a44
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-05-05 22:55:40 -0300

    Update

--- 6-Native-KeyFrame/rnkeyframes/App.tsx
+++ 6-Native-KeyFrame/rnkeyframes/App.tsx
@@ -1,12 +1,22 @@
-import { View } from "react-native";
+import { Button, View } from "react-native";
 import { Message } from "./src/components/Message";
 
 import { styles } from "./styles";
+import { useState } from "react";
 
 export default function App() {
+const [show, setShow] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Message />
+
+
+      {show && <Message />}
+
+      <Button 
+      title={show ? "Fechar Mensagem" : "Exibir Mensagem"}
+      onPress={() => setShow((prevState => !prevState))}
+      />
     </View>
   );
 }
