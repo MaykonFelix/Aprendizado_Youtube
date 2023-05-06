e1740086e0a91bb66cc6fbadfd67b4ddcb47b3aa
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-05-05 23:07:14 -0300

    Update

--- 6-Native-KeyFrame/rnkeyframes/src/components/Message/index.tsx
+++ 6-Native-KeyFrame/rnkeyframes/src/components/Message/index.tsx
@@ -1,13 +1,23 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import Animated, { Keyframe } from "react-native-reanimated";
 
 import { styles } from "./styles";
 
 export function Message() {
+  const enteringKeyframe = new Keyframe({
+    0: { opacity: 0, transform: [{ translateY: -100 }] },
+    70: { opacity: 0.3 },
+    100: { opacity: 1, transform: [{ translateY: 0 }] },
+  });
+
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={styles.container}
+      entering={enteringKeyframe.duration(500)}
+    >
       <MaterialIcons name="notifications" color="#FFF" size={18} />
       <Text style={styles.title}>Mensagem de Exemplo.</Text>
-    </View>
+    </Animated.View>
   );
 }
