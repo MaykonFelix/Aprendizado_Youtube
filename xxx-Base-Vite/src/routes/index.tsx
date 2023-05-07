9d84fdeb72c82cfa12d5623f2b21b9fd496b3c5f
Maykon Felix <equipefelixx@gmail.com>
2023-05-07 17:58:26 -0300

    Update

---
+++ xxx-Base-Vite/src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<p>Pagina Inicial</p>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
