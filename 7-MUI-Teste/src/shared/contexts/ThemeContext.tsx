ee925ea12a0e30fb907b833de1c1e12bfc196459
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-05-10 13:15:31 -0300

    Update

--- 7-MUI-Teste/src/shared/contexts/ThemeContext.tsx
+++ 7-MUI-Teste/src/shared/contexts/ThemeContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, useCallback, useContext, useMemo, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 
 import { DarkTheme, LightTheme } from "../themes";
 
@@ -11,12 +17,17 @@ interface IThemeContextData {
 
 const ThemeContext = createContext({} as IThemeContextData);
 
-export const useAppThemeContext = () =>{
+export const useAppThemeContext = () => {
   return useContext(ThemeContext);
+};
+
+interface IAppThemeProviderProps {
+  children: React.ReactNode;
 }
 
-
-export const AppThemeProvider: React.FC = ({ children }) => {
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({
+  children,
+}) => {
   const [themeName, setThemeName] = useState<"light" | "dark">("light");
 
   const toggleTheme = useCallback(() => {
@@ -34,7 +45,11 @@ export const AppThemeProvider: React.FC = ({ children }) => {
   return (
     <ThemeContext.Provider value={{ themeName, toggleTheme }}>
       <ThemeProvider theme={theme}>
-        <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+        <Box
+          width="100vw"
+          height="100vh"
+          bgcolor={theme.palette.background.default}
+        >
           {children}
         </Box>
       </ThemeProvider>
