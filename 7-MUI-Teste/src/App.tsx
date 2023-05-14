f96a36009fca91d643638f48a141005b7037c19d
Maykon Felix <equipefelixx@gmail.com>
2023-05-14 17:16:39 -0300

    feat: menu lateral

--- 7-MUI-Teste/src/App.tsx
+++ 7-MUI-Teste/src/App.tsx
@@ -4,10 +4,15 @@ import { AppThemeProvider } from "./shared/contexts/index";
 
 import { AppRoutes } from "./routes";
 
+import { MenuLateral } from "./shared/components";
+
+
+
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
+        <MenuLateral />
         <AppRoutes />
       </BrowserRouter>
     </AppThemeProvider>
